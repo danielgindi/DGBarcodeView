c48d8324547e2bd596d0520d062ef23e084669ac
Daniel Cohen Gindi <danielgindi@gmail.com>
2014-10-19 10:19:23 +0300

    Initial code commit

---
+++ Encoders/DGBarcodeUPCSupplement5Encoder.h
@@ -0,0 +1,17 @@
+//
+//  DGBarcodeUPCSupplement5Encoder.h
+//  DGBarcodeView
+//
+//  Created by Daniel Cohen Gindi on 1/31/13.
+//  Copyright (c) 2013 Daniel Cohen Gindi. All rights reserved.
+//
+//  https://github.com/danielgindi/DGBarcodeView
+//
+
+#import "DGBarcodeEncoder.h"
+
+@interface DGBarcodeUPCSupplement5Encoder : DGBarcodeEncoder
+
+- (NSString*)encodedValueWithValue:(NSString*)value;
+
+@end
